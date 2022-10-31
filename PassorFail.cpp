6bc9e61c2c75361e60c6ed24a9626e1000c0082a
naincyrani06 <68509153+naincyrani06@users.noreply.github.com>
2022-10-31 20:24:10 +0530

    Create PassorFail.cpp

---
+++ PassorFail.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int32_t main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n , x , p;
+         cin>>n>>x>>p;
+         if ((x*3-(n-x)>=p)) {
+     cout<< "PASS"<<"\n";
+    }
+    else{
+        cout <<"FAIL"<<"\n";
+        }
+    }
+    return 0;
+    }
