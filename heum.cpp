c7a0052b77da0ce3bf8d22f3df0d5a7238701d50
Shubham Singh <87699417+detonators@users.noreply.github.com>
2022-10-31 21:02:57 +0530

    new

---
+++ heum.cpp
@@ -0,0 +1,21 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+int32_t main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n ;
+         cin>>n;
+         if (n>=9)) 
+         {
+     cout<< "PASS"<<"\n";
+         }
+    else{
+        cout <<"FAIL"<<"\n";
+        }
+    }
+    return 0;
+    }
